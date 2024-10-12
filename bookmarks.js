a7c64a249515b8ddeb803d4ff05ab94f135cb0f5
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-10-12 12:34:48 +0530

    first commit

---
+++ bookmarks.js
@@ -0,0 +1,45 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      {name: "GitHub", url: "https://github.com"},
+      {name: "Figma", url: "https://figma.com"},
+      {name: "Inbox", url: "https://inbox.google.com"},
+      {name: "Drive", url: "https://drive.google.com"},
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      {name: "Youtube", url: "https://youtube.com"},
+      {name: "AniWatch", url: "https://aniwatch.to/home"},
+    ],
+  },
+  // {
+  //   title: "Reddit",
+  //   links: [
+  //     {name: "/r/overwatch", url: "https://reddit.com/r/overwatch"},
+  //     {
+  //       name: "/r/pcmasterrace",
+  //       url: "https://reddit.com/r/pcmasterrace",
+  //     },
+  //     {name: "/r/me_irl", url: "https://reddit.com/r/me_irl"},
+  //     {
+  //       name: "/r/battlestations",
+  //       url: "https://reddit.com/r/battlestations",
+  //     },
+  //     {name: "/r/unixporn", url: "https://reddit.com/r/unixporn"},
+  //     {name: "/r/news", url: "https://reddit.com/r/news"},
+  //   ],
+  // },
+  {
+    title: "Social",
+    links: [
+      {name: "Instagram", url: "https://instagram.com"},
+      {name: "InternShala", url: "https://internshala.com/student/dashboard"},
+      {name: "Twitter", url: "https://twitter.com"},
+      {name: "Facebook", url: "https://facebook.com"},
+    ],
+  },
+];
